b6649906860ec22855e99c0a017e3cc2ee342f1d
German Dolnikov <german@dolnikov.ru>
2020-02-16 08:07:36 +0300

    Правки линтера

--- js/customization.js
+++ js/customization.js
@@ -1,107 +1,107 @@
 'use strict';
 
 (function () {
-// Функция получения случайного целого числа в заданном интервале. Максимум и минимум включаются
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+  // Функция получения случайного целого числа в заданном интервале. Максимум и минимум включаются
+  function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
 
-// Создаю «Похожих персонажей»
-var names = [
-  'Иван',
-  'Хуан Себастьян',
-  'Мария',
-  'Кристоф',
-  'Виктор',
-  'Юлия',
-  'Люпита',
-  'Вашингтон'];
+  // Создаю «Похожих персонажей»
+  var names = [
+    'Иван',
+    'Хуан Себастьян',
+    'Мария',
+    'Кристоф',
+    'Виктор',
+    'Юлия',
+    'Люпита',
+    'Вашингтон'];
 
-var secondNames = [
-  'да Марья',
-  'Верон',
-  'Мирабелла',
-  'Вальц',
-  'Онопко',
-  'Топольницкая',
-  'Нионго',
-  'Ирвинг',
-];
+  var secondNames = [
+    'да Марья',
+    'Верон',
+    'Мирабелла',
+    'Вальц',
+    'Онопко',
+    'Топольницкая',
+    'Нионго',
+    'Ирвинг',
+  ];
 
-var coatColors = [
-  'rgb(101, 137, 164)',
-  'rgb(241, 43, 107)',
-  'rgb(146, 100, 161)',
-  'rgb(56, 159, 117)',
-  'rgb(215, 210, 55)',
-  'rgb(0, 0, 0)',
-];
+  var coatColors = [
+    'rgb(101, 137, 164)',
+    'rgb(241, 43, 107)',
+    'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',
+    'rgb(215, 210, 55)',
+    'rgb(0, 0, 0)',
+  ];
 
-var eyesColors = [
-  'black',
-  'red',
-  'blue',
-  'yellow',
-  'green',
-];
+  var eyesColors = [
+    'black',
+    'red',
+    'blue',
+    'yellow',
+    'green',
+  ];
 
-var fireballColors = [
-  '#ee4830',
-  '#30a8ee',
-  '#5ce6c0',
-  '#e848d5',
-  '#e6e848',
-];
+  var fireballColors = [
+    '#ee4830',
+    '#30a8ee',
+    '#5ce6c0',
+    '#e848d5',
+    '#e6e848',
+  ];
 
-var WizzardOptions = [];
+  var WizzardOptions = [];
 
-for (var i = 1; i < 5; i++) {
-  var namePart = names[getRandomIntInclusive(0, 7)];
-  var secondNamePart = secondNames[getRandomIntInclusive(0, 7)];
-  WizzardOptions.push({
-    name: namePart + ' ' + secondNamePart,
-    coatColor: coatColors[getRandomIntInclusive(0, coatColors.length - 1)],
-    eyesColor: eyesColors[getRandomIntInclusive(0, eyesColors.length - 1)],
-  });
-}
-
-
-// Меняю цвет плаща по клику
-var wizardCoat = document.querySelector('.setup-wizard .wizard-coat');
-var setupWizardAppearance = document.querySelectorAll('.setup-wizard-appearance input');
-
-var wizardCoatClickHandler = function () {
-  var coatColorsRandom = coatColors[getRandomIntInclusive(0, coatColors.length - 1)];
-  wizardCoat.style.fill = coatColorsRandom;
-  setupWizardAppearance[0].value = coatColorsRandom;
-};
-
-wizardCoat.addEventListener('click', wizardCoatClickHandler);
+  for (var i = 1; i < 5; i++) {
+    var namePart = names[getRandomIntInclusive(0, 7)];
+    var secondNamePart = secondNames[getRandomIntInclusive(0, 7)];
+    WizzardOptions.push({
+      name: namePart + ' ' + secondNamePart,
+      coatColor: coatColors[getRandomIntInclusive(0, coatColors.length - 1)],
+      eyesColor: eyesColors[getRandomIntInclusive(0, eyesColors.length - 1)],
+    });
+  }
 
 
-// Меняю цвет глаз по клику
-var wizardEyes = document.querySelector('.setup-wizard .wizard-eyes');
+  // Меняю цвет плаща по клику
+  var wizardCoat = document.querySelector('.setup-wizard .wizard-coat');
+  var setupWizardAppearance = document.querySelectorAll('.setup-wizard-appearance input');
 
-var wizardEyesClickHandler = function () {
-  var eyesColorsRandom = eyesColors[getRandomIntInclusive(0, eyesColors.length - 1)];
-  wizardEyes.style.fill = eyesColorsRandom;
-  setupWizardAppearance[1].value = eyesColorsRandom;
-};
+  var wizardCoatClickHandler = function () {
+    var coatColorsRandom = coatColors[getRandomIntInclusive(0, coatColors.length - 1)];
+    wizardCoat.style.fill = coatColorsRandom;
+    setupWizardAppearance[0].value = coatColorsRandom;
+  };
 
-wizardEyes.addEventListener('click', wizardEyesClickHandler);
+  wizardCoat.addEventListener('click', wizardCoatClickHandler);
 
 
-// Меняю цвет файрбола по клику
-var fireballWrap = document.querySelector('.setup-fireball-wrap');
+  // Меняю цвет глаз по клику
+  var wizardEyes = document.querySelector('.setup-wizard .wizard-eyes');
 
-var fireballWrapClickHandler = function () {
-  var fireballColor = fireballColors[getRandomIntInclusive(0, fireballColors.length - 1)];
-  fireballWrap.style.backgroundColor = fireballColor;
-  fireballWrap.querySelector('input').value = fireballColor;
-};
+  var wizardEyesClickHandler = function () {
+    var eyesColorsRandom = eyesColors[getRandomIntInclusive(0, eyesColors.length - 1)];
+    wizardEyes.style.fill = eyesColorsRandom;
+    setupWizardAppearance[1].value = eyesColorsRandom;
+  };
 
-fireballWrap.addEventListener('click', fireballWrapClickHandler);
-}) ();
+  wizardEyes.addEventListener('click', wizardEyesClickHandler);
+
+
+  // Меняю цвет файрбола по клику
+  var fireballWrap = document.querySelector('.setup-fireball-wrap');
+
+  var fireballWrapClickHandler = function () {
+    var fireballColor = fireballColors[getRandomIntInclusive(0, fireballColors.length - 1)];
+    fireballWrap.style.backgroundColor = fireballColor;
+    fireballWrap.querySelector('input').value = fireballColor;
+  };
+
+  fireballWrap.addEventListener('click', fireballWrapClickHandler);
+})();
--- js/utils.js
+++ js/utils.js
@@ -2,65 +2,65 @@
 
 (function () {
 // Открываю форму по иконке
-var setup = document.querySelector('.setup');
-var setupOpen = document.querySelector('.setup-open');
+  var setup = document.querySelector('.setup');
+  var setupOpen = document.querySelector('.setup-open');
 
-var setupOpenClickHandler = function () {
-  setup.classList.remove('hidden');
-};
-
-setupOpen.addEventListener('click', setupOpenClickHandler);
-
-
-// По Enter открываю форму
-var setupOpenIcon = document.querySelector('.setup-open-icon');
-
-var setupOpenIconKeydownHandler = function (evt) {
-  if (evt.key === 'Enter') {
+  var setupOpenClickHandler = function () {
     setup.classList.remove('hidden');
-  }
-};
+  };
 
-setupOpenIcon.addEventListener('keydown', setupOpenIconKeydownHandler);
-
-
-// Закрываю форму на крестике
-var setupClose = document.querySelector('.setup-close');
-
-var setupCloseClickHandler = function () {
-  setup.classList.add('hidden');
-};
-
-setupClose.addEventListener('click', setupCloseClickHandler);
+  setupOpen.addEventListener('click', setupOpenClickHandler);
 
 
-// По Enter на крестике закрываю форму
-var setupCloseKeydownEnterHandler = function (evt) {
-  if (evt.key === 'Enter') {
+  // По Enter открываю форму
+  var setupOpenIcon = document.querySelector('.setup-open-icon');
+
+  var setupOpenIconKeydownHandler = function (evt) {
+    if (evt.key === 'Enter') {
+      setup.classList.remove('hidden');
+    }
+  };
+
+  setupOpenIcon.addEventListener('keydown', setupOpenIconKeydownHandler);
+
+
+  // Закрываю форму на крестике
+  var setupClose = document.querySelector('.setup-close');
+
+  var setupCloseClickHandler = function () {
     setup.classList.add('hidden');
-  }
-};
+  };
 
-setupClose.addEventListener('keydown', setupCloseKeydownEnterHandler);
-
-// Esc закрывает форму
-var setupCloseKeydownHandler = function (evt) {
-  if (evt.key === 'Escape') {
-    setup.classList.add('hidden');
-  }
-};
-
-document.addEventListener('keydown', setupCloseKeydownHandler);
+  setupClose.addEventListener('click', setupCloseClickHandler);
 
 
-// Esc не закрыват форму на поле ввода
-var setupUserName = document.querySelector('.setup-user-name');
+  // По Enter на крестике закрываю форму
+  var setupCloseKeydownEnterHandler = function (evt) {
+    if (evt.key === 'Enter') {
+      setup.classList.add('hidden');
+    }
+  };
 
-var setupUserNameKeydownHandler = function (evt) {
-  if (evt.key === 'Escape') {
-    evt.stopPropagation();
-  }
-};
+  setupClose.addEventListener('keydown', setupCloseKeydownEnterHandler);
 
-setupUserName.addEventListener('keydown', setupUserNameKeydownHandler);
-}) ();
+  // Esc закрывает форму
+  var setupCloseKeydownHandler = function (evt) {
+    if (evt.key === 'Escape') {
+      setup.classList.add('hidden');
+    }
+  };
+
+  document.addEventListener('keydown', setupCloseKeydownHandler);
+
+
+  // Esc не закрыват форму на поле ввода
+  var setupUserName = document.querySelector('.setup-user-name');
+
+  var setupUserNameKeydownHandler = function (evt) {
+    if (evt.key === 'Escape') {
+      evt.stopPropagation();
+    }
+  };
+
+  setupUserName.addEventListener('keydown', setupUserNameKeydownHandler);
+})();
